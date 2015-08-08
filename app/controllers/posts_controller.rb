660bd883ba1d0b3630085696784eb7a6131eb098
droboshok <droboshok@yahoo.com>
2015-08-08 23:44:48 +0300

    Add User

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, exept: [:index, :show]
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -9,11 +9,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post
