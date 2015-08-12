9b2b1b83e4f5d3db8b1d5a671f4159cc887e6a2b
droboshok <droboshok@yahoo.com>
2015-08-12 20:18:53 +0300

    Setup peperclip image uploading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,7 @@ Rails.application.configure do
 
   #Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  #Pepirclip
+  
+  Paperclip.options[:command_path] = "/usr/bin/convert"
 end
